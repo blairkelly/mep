bf2fc2eccf4d5a6d6f3644b3726c349995f9d4bc
Blair Kelly <blairkelly1@gmail.com>
2015-10-17 09:02:02 -0400

    now including images in photographs folder yay

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 var app = module.parent.exports;
+var path = require('path');
+var find = require('find');
+
+var photographs = [];
+
+//photographs
+var photographs_folder = path.resolve(__dirname + '/../public/images/photographs/');
+find.fileSync(/\.(gif|jpg|jpeg|tiff|png)$/i, photographs_folder).forEach(function (imgfile) {
+    photographs.push(imgfile.substr(photographs_folder.length + 1, imgfile.length));
+});
 
 app.get('/', app.mainMiddleware, function (req, res, next) {
+    res.locals.photographs = photographs;
+    res.locals.photographs_stringified = JSON.stringify(photographs, null, 6);
     res.render('index/index.jade');
 });
